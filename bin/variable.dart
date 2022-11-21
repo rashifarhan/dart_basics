e8d601e8e6168009d7f742ee3158a0845b42e884
Rashi farhan <rashifarhan90@gmail.com>
2022-11-21 13:18:27 +0530

    commit

---
+++ bin/variable.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  String mobile="motorolla g40 fusion";
+  int ram=4;
+  double price=12333.0;
+  String color="Gold";
+  int storage=64;
+  print("____my phone details____");
+  print("name:$mobile");
+  print("Ram:$ram Gb");
+  print("color:$color");
+  print("storage:$storage Gb");
+}
