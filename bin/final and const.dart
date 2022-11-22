c5f5ed37517ccfde02736be08106f7ee30269ccb
Rashi farhan <rashifarhan90@gmail.com>
2022-11-22 13:34:01 +0530

    commit

---
+++ bin/final and const.dart
@@ -0,0 +1,12 @@
+void main(){
+  var name="anu";
+  var age=20;
+  final course="flutter";
+  print(name);
+  print(age);
+  print(course);
+  const a="rrr";
+  print(a);
+  //final fix the value at the time of allocation
+  //const fix the value at the time of compilation
+}
