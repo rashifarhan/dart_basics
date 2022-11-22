6a3d7ec8510a1c1f6e6ef8a2f77da731a9a4d9ff
Rashi farhan <rashifarhan90@gmail.com>
2022-11-22 13:23:20 +0530

    commit

---
+++ bin/variable and dynamic.dart
@@ -0,0 +1,11 @@
+void main(){
+  String name="anu";
+  var lastname="paul";
+  print("$name $lastname");
+  lastname="prasadh";
+  print("$name $lastname");
+  dynamic age=3;
+  age="six";
+  print(age);
+
+}
