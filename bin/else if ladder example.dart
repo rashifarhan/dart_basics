3ec4a60a2d396b73424055b548560e580055beb4
Rashi farhan <rashifarhan90@gmail.com>
2022-11-25 10:25:39 +0530

    commit

---
+++ bin/else if ladder example.dart
@@ -0,0 +1,15 @@
+void main(){
+  var mysize="M";
+  if(mysize=="S"){
+    print("wrong size");
+  }
+  else if(mysize=="M"){
+    print("correct size");
+  }
+  else if(mysize=="XL"){
+    print("wrong size");
+  }
+  else{
+    print("not found");
+  }
+}
