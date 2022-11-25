3bd8ffe425bf124a3255a445fa7a4020f6c1b9b7
Rashi farhan <rashifarhan90@gmail.com>
2022-11-25 10:11:36 +0530

    commit

---
+++ bin/nested if else example.dart
@@ -0,0 +1,17 @@
+void main(){
+  var mark=76;
+
+  if(mark>=40){
+
+    if(mark>=80){
+      print("distinction");
+    }
+    else{
+      print("pass without distinction");
+    }
+  }
+  else{
+    print("failed");
+  }
+
+}
