800b56c6bb499255033675a9b2f66b63671bd604
Rashi farhan <rashifarhan90@gmail.com>
2022-11-25 09:50:41 +0530

    commit

--- bin/if else.dart
+++ bin/if else.dart
@@ -1,9 +1,20 @@
-void main(){
-  var age=20;
-  if (age>=18){
-    print("eligible to vote"); }
-  else{
+void main() {
+  var age = 20;
+  if (age >= 18) {
+    print("eligible to vote");
+  }
+  else {
     print("not eligible");
   }
 
+
+//largest of two number
+  int a = 10;
+  int b = 25;
+  if (a > b) {
+    print("$a is largest");
+  }
+  else{
+    print("$b is largset");
+  }
 }
