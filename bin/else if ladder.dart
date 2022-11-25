4eebddc5e49d9ee172f83adacebe11555bf3e4e3
Rashi farhan <rashifarhan90@gmail.com>
2022-11-25 10:20:14 +0530

    commit

---
+++ bin/else if ladder.dart
@@ -0,0 +1,19 @@
+void main(){
+  var month="november";
+  if(month=="january"){
+    print("january");
+  }
+  else if(month=="february"){
+    print("february");
+  }
+  else if(month=="march"){
+    print("march");
+  }
+  else if(month=="november"){
+    print("november");
+  }
+  else{
+    print("none of this");
+  }
+}
+///only print first detected true statement
