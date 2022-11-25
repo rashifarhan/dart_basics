5ff4b9e7dd1ee512a11e1a4b6e64cbb08b197913
Rashi farhan <rashifarhan90@gmail.com>
2022-11-25 10:37:52 +0530

    commit

---
+++ bin/largest of four.dart
@@ -0,0 +1,21 @@
+void main(){
+  var a=20;
+  var b=10;
+  var c=4;
+  var d=5;
+  var largest;
+  if(a>b && a>c && a>d){
+    largest=a;
+  } 
+  else if( b>c && b>d){
+    largest=b;
+  }
+  else if(c>d){
+    largest=c;
+  }
+  else{
+    largest=d;
+  }
+  print("largest:$largest");
+
+}
