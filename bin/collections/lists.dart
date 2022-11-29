b1259f20b307bd0a05e11dcb2fb69232fac24737
Rashi farhan <rashifarhan90@gmail.com>
2022-11-29 09:28:19 +0530

    commit

---
+++ bin/collections/lists.dart
@@ -0,0 +1,11 @@
+void main(){
+  List<String> mylist=['a','e','y','9','i'];//list
+  print(mylist);
+  ///List<String> List1=[values];
+  ///var list2=<int>[values];
+  for(int index=0;index<mylist.length;index++){
+    print(mylist[0]);
+  }
+
+
+}
