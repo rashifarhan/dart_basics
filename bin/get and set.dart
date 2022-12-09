b3ef8598a7445a38176c7d639c52186f90d27b1d
Rashi farhan <rashifarhan90@gmail.com>
2022-12-09 09:56:31 +0530

    commit

---
+++ bin/get and set.dart
@@ -0,0 +1,16 @@
+class Encapsulation{
+  late String name;
+  late int age;
+  String get myname{
+    return name;
+  }
+  int get myage{
+    return age;
+  }
+  set myname(String name){
+    this.name=name;
+  }
+  set myage(int age){
+    this.age=age;
+  }
+}
